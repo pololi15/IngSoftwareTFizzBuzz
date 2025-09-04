3b7a1f2eabcec0a6c6a3e9bf8a1b3c27eff32bf1
Ostin <ostinrivas15@gmail.com>
2025-09-04 01:42:18 -0400

    feat: devuelve 2 si la entrada es 2

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -4,5 +4,7 @@ describe('fizzbuzz', () => {
     it('deberia devolver "1" si le pongo de entrada 1', () => {
         expect(fizzbuzz(1)).toEqual(1);
     });
-
+    it('deberia devolver "2" si le pongo de entrada 2', () => {
+        expect(fizzbuzz(2)).toEqual(2);
+    });
 });
